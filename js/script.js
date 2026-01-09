5427753653cbf15420a36f266b10c10884beab28
衣川颯姫 <>
2026-01-09 13:28:21 +0900

    専攻絞り込みjs 完成

--- js/script.js
+++ js/script.js
@@ -2,7 +2,25 @@
 
 
 $(function () {
-    
+
+    $('.filter-btn[data-filter="all"]').addClass('btn-on');
 
     
+    $('.filter-btn').on('click', function () {
+    const filter = $(this).data('filter');
+
+    // active切り替え
+    $('.filter-btn').removeClass('btn-on');
+    $(this).addClass('btn-on');
+
+    // 表示切り替え
+    if (filter === 'all') {
+      $('.chara-block li').show();
+    } else {
+      $('.chara-block li').hide();
+      $('.chara-block li[data-category="' + filter + '"]').show();
+    }
+  });
+
+
 });
