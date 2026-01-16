97143948672cc431217f539dacd33b8716d6b1c1
衣川颯姫 <>
2026-01-17 01:23:31 +0900

    データ打ち込み完了　残り：イラスト

--- js/set.js
+++ js/set.js
@@ -4,6 +4,23 @@
 
 const chara_prof = {
 
+    "青田": {
+
+        name: "みそすーぷ",
+        major: "Gra",
+        className: "gra",
+        image: "img/chara_angelica.png",
+        image02: "img/chara_angelica02.png",
+        food: "お寿司（エンガワ）",
+        want: "かわいい服",
+        music: "「ネビュラ」",
+        music_text: "",
+        iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/53CPxuXz72o?si=m3fo9dgOxxxGsiCa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        kd: "楽しい",
+        free: "　"
+
+    },
+
     "アンジェリカ": {
 
         name: "アンジェリカ",
@@ -35,6 +52,91 @@ const chara_prof = {
         iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Ouy9d6-r9nQ?si=OQPndlqQfnv9pJ3y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
         kd: "面白い先生が沢山いるところが魅力！",
         free: "やる気、元気、純喜！！"
+
+    },
+
+    "河田": {
+
+        name: "カワティー",
+        major: "Web",
+        image: "img/chara_utasiro.png",
+        image02: "img/chara_utasiro02.png",
+        className: "web",
+        food: "あんこ(特につぶあん)",
+        want: "冬物の服！",
+        music: "「Tempalay - Magic」",
+        music_text: '少し前にEP(シングルより多くアルバムより少ないアルバムのようなもの)をリリースした時に、入っていた新曲です。Tempalayは曲によってかなり曲調が変わるのですが、この「Magic」は穏やかな曲調になります。日常生活のふとした場面を愛おしく感じられるような音楽で凄く好きでリピートしています。<br>※もしオルタナティブ系の音楽も普段聴かれる方がいれば同じくTempalayで特にオススメしたいのは、<a class="a-url" href="https://youtu.be/SmkSKrvGWxE?si=ui5cmRAW85NQawY8">「続・JOE」</a>、<a class="a-url" href="https://youtu.be/g76DxlDzSV4?si=NZLu2TQ5t4y8JccA">「かみんち」</a>、<a class="a-url" href="https://youtu.be/XbfGem5GN7M?si=bdwpGPM03Xlk78Hz">「のめりこめ震えろ」</a>オススメです！！他にもめっちゃ良いのあるのですが、3曲に絞らせてもらいました‥！Tempalay本当に良いのでぜひぜひ聴いてみてください～！！',
+        iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Jvz1B3im0Q8?si=czRXXY7giTirEyIe" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        kd: "先生方が個性はあるけどめっちゃ優しい先生たちです！デザインだけでなく、物事の考え方とかなるほどなと勉強になることが多く(特に小畑先生)入学して良かったなと思いました！",
+        free: "友達の卒業制作にキャラクターとして参加させてもらえて嬉しいです‼️"
+
+    },
+
+    "金田": {
+
+        name: "茶碗蒸し",
+        major: "Gra",
+        image: "img/chara_utasiro.png",
+        image02: "img/chara_utasiro02.png",
+        className: "gra",
+        food: "桃、マカロン、激辛料理",
+        want: "merryjennyの服",
+        music: "「雨降る夜の向こう」",
+        music_text: "",
+        iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/AEzNWhWfeIg?si=HeqzXpdBQ3DoADZz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        kd: "先生がフレンドリーで話しやすい。建物が綺麗。",
+        free: "　"
+
+    },
+
+    "後藤": {
+
+        name: "まっちゃぷりん",
+        major: "Gra",
+        image: "img/chara_utasiro.png",
+        image02: "img/chara_utasiro02.png",
+        className: "gra",
+        food: "サーモン寿司",
+        want: "可愛い靴",
+        music: "「MELLOW DEAR US - Dear World」",
+        music_text: "",
+        iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/GYJ_JJfT7Vs?si=TpVQfFHigQJ9uQr3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        kd: "現役で活躍されている方に実際にお話を聞ける機会があるところ！",
+        free: "　"
+
+    },
+
+    "才田": {
+
+        name: "さいちゃん",
+        major: "Gra",
+        image: "img/chara_utasiro.png",
+        image02: "img/chara_utasiro02.png",
+        className: "gra",
+        food: "寿司（サーモン）",
+        want: "・オタマトーン・WOSADOのつけま",
+        music: "「CUTIE STREET - ひたむきシンデレラ！」",
+        music_text: 'CUTIE STREETをよろしくお願いします。冬っぽい曲。ぱるたん推し。<br><a class="a-url" href="https://youtu.be/dfFfMRC3E1g?si=CUpFGa6tdE4xdRhO">「STAND PROUD」</a>：ジョジョ３部YouTubeで無料公開中です。私は４部が１番好き。<br><a class="a-url" href="https://youtu.be/lTUVw7wd5qY?si=ZMKSQlWNe5Q8pL3r">「ユーチューバー」</a>：ピノキオピーサイコー！サビが耳に永住。',
+        iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/AtDErGSpfYQ?si=BoRXyEMsH5FwgdvD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        kd: "外部イベントに参加できる機会が多いかもしれない",
+        free: "　"
+
+    },
+
+    "坂口": {
+
+        name: "サカグチ",
+        major: "Web",
+        image: "img/chara_utasiro.png",
+        image02: "img/chara_utasiro02.png",
+        className: "web",
+        food: "アボカド",
+        want: "大きい収納",
+        music: "「Daichi Yamamoto - メルセデス」",
+        music_text: "",
+        iframe: '',
+        kd: "自由な制作ができる",
+        free: "祇園先生のお気に入り"
 
     },
 
@@ -72,6 +174,74 @@ const chara_prof = {
 
     },
 
+    "寺本": {
+
+        name: "てら",
+        major: "Web",
+        image: "img/chara_tein.png",
+        image02: "img/chara_tein02.png",
+        className: "web",
+        food: "アイスクリーム",
+        want: "Switch2　ポケモンレジェンズZA",
+        music: '<a class="a-url" href="https://youtu.be/sNikwz23upE?si=dLWGmODKnQGhxia1">「SEKAI NO OWARI - サラバ」</a>',
+        music_text: "過去の苦痛に一緒に寄り添ってくれ、新しい一歩を踏み出すのに背中を押してくれるような、そんな曲です。明るいメロディで歌詞が優しい。",
+        iframe: '',
+        kd: "先生方との距離が近くて気軽に話せること！",
+        free: "チョコミントは歯磨き粉じゃない...よね...？"
+
+    },
+
+    "手嶋": {
+
+        name: "てっしー",
+        major: "Web",
+        image: "img/chara_tein.png",
+        image02: "img/chara_tein02.png",
+        className: "web",
+        food: "からあげ",
+        want: "ライブのチケット",
+        music: "「Official髭男dism - Tell Me Baby」",
+        music_text: "オシャレでかっこいい曲です！ライブVer.のアレンジもかっこいいので、是非聴いてみてほしいです！",
+        iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/3IDvi4buNdk?si=si2I4jJl7Y8PGAEl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        kd: "学校付近に美味しいご飯屋さんがあるよ！",
+        free: "笑顔、泣き顔、長尾！！"
+
+    },
+
+    "春名": {
+
+        name: "はるな",
+        major: "Web",
+        image: "img/chara_matumoto_e.png",
+        image02: "img/chara_matumoto_e02.png",
+        className: "web",
+        food: "ケーキ🍰",
+        want: "でかいオーブン",
+        music: '<a class="a-url" href="https://music.youtube.com/watch?v=dUs28xUmX9I&si=QIkNdUmPJVfSFskD">「宇多田ヒカル - 道」</a>',
+        music_text: "",
+        iframe: '',
+        kd: "先生の個性が強くて面白い",
+        free: "　"
+
+    },
+
+    "藤井": {
+
+        name: "どかち",
+        major: "Web",
+        image: "img/chara_matumoto_e.png",
+        image02: "img/chara_matumoto_e02.png",
+        className: "web",
+        food: "うどん",
+        want: "冬服！全く足りない！",
+        music: "「Vaundy - 常熱」",
+        music_text: "",
+        iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/9UnIdLcfeAw?si=3U6g95NbciLybp40" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        kd: "先生たちが個性豊かで話しやすい！あとは駅前で寄り道しやすいのが嬉しい",
+        free: "いつでも話しかけて下さい〜！"
+
+    },
+
     "松本えり": {
 
         name: "えりぴ",
@@ -89,6 +259,40 @@ const chara_prof = {
 
     },
 
+    "松本わかば": {
+
+        name: "わかたん",
+        major: "Web",
+        image: "img/chara_matumoto_e.png",
+        image02: "img/chara_matumoto_e02.png",
+        className: "web",
+        food: "醤油ラーメン",
+        want: "茶色のムートンブーツ",
+        music: "「Mrs. GREEN APPLE - ダンスホール」",
+        music_text: "歌詞に「メンタルも成長痛を起こすでしょう 無理をせず自分らしくいて」というフレーズを聴くと、私自身とても励まされるから。",
+        iframe: '',
+        kd: "動画コースの授業では企画から撮影、編集まで一貫して行っているところが実践的且つ魅力的だと思います。",
+        free: "　"
+
+    },
+
+    "矢田貝": {
+
+        name: "さーや",
+        major: "Web",
+        image: "img/chara_matumoto_e.png",
+        image02: "img/chara_matumoto_e02.png",
+        className: "web",
+        food: "白米",
+        want: "イヤホン　「最近壊れちゃった、、。",
+        music: "「NCT WISH - Our Adventures」",
+        music_text: "",
+        iframe: '',
+        kd: "みんな仲がいい",
+        free: "K-pop大好き人間"
+
+    },
+
     "岡村先生": {
 
         name: "おかむー",
@@ -102,6 +306,40 @@ const chara_prof = {
         music_text: "イントロのカッティングでテンション爆上がりです！",
         iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/sxB76walfnE?si=j06U0DpybZyY900R" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
         kd: "いろんなことに挑戦できる環境が揃ってます！",
+        free: "　"
+
+    },
+
+    "西口先生": {
+
+        name: "しょお",
+        major: "先生",
+        image: "img/chara_okamura.png",
+        image02: "img/chara_okamura02.png",
+        className: "teacher",
+        food: "らーめんちゃーはん餃子",
+        want: "電動バイク",
+        music: "いまビックバンを聞いている",
+        music_text: "",
+        iframe: '',
+        kd: "体力を削る坂",
+        free: "神戸電子にきてもう11年が経ちました。"
+
+    },
+
+    "宮脇先生": {
+
+        name: "みやわき",
+        major: "先生",
+        image: "img/chara_okamura.png",
+        image02: "img/chara_okamura02.png",
+        className: "teacher",
+        food: "オムライス",
+        want: "山のようにある工作用の素材を綺麗にしまえる棚…",
+        music: "「Snail's House - インベーダー☆」",
+        music_text: "大好きでずっと聴いています。元気がでます😊",
+        iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/jezqbMVqcLk?si=SoHyL_L52znJzUY2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        kd: "先生たちも、学生のみんなもあたたかい人ばかりです。だれかの「これがやりたい！」という気持ちに寄り添える場所だと思います。自分のやりたいことにたくさん挑戦させてもらえるので、教員ではありますが私も年々成長し続けている気がします！",
         free: "　"
 
     },
