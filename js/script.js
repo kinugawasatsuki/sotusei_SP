f69afde03b1ac25f405e4b68ee4f5a268d0cd418
衣川颯姫 <>
2026-01-16 14:03:43 +0900

    charaページ　js

--- js/script.js
+++ js/script.js
@@ -3,29 +3,71 @@
 
 $(function () {
 
-  $('.filter-btn[data-filter="all"]').addClass('btn-on');
+  if ($('.main-wrapper').length) {
 
 
-  $('.filter-btn').on('click', function () {
-    const filter = $(this).data('filter');
+    $('.filter-btn[data-filter="all"]').addClass('btn-on');
 
-    // active切り替え
-    $('.filter-btn').removeClass('btn-on');
-    $(this).addClass('btn-on');
 
-    // 表示切り替え
-    if (filter === 'all') {
-      $('.chara-block li').show();
-    } else {
-      $('.chara-block li').hide();
-      $('.chara-block li[data-category="' + filter + '"]').show();
-    }
-  });
+    $('.filter-btn').on('click', function () {
+      const filter = $(this).data('filter');
 
-  $('.rotate').on('click', function () {
-    $('.chara-image').toggleClass('rotate-on');
-    console.log('aaa');
-  });
+      // active切り替え
+      $('.filter-btn').removeClass('btn-on');
+      $(this).addClass('btn-on');
 
+      // 表示切り替え
+      if (filter === 'all') {
+        $('.chara-block li').show();
+      } else {
+        $('.chara-block li').hide();
+        $('.chara-block li[data-category="' + filter + '"]').show();
+      }
+    });
+
+    
+
+  }
+
+});
+
+$(function () {
+
+  if ($('.chara-wrapper').length) {
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const chara = urlParams.get('chara');
+    const data = chara_prof[chara];
+
+    console.log("chara:", chara);
+    console.log("data:", data);
+
+
+    $('.chara-image').html(`
+      <img class="c-img c-main" src="${data.image}"
+      <img class="c-img c-hover" src="${data.image_02}"`);
+    $('.name').text(data.name);
+    $('.major').text(data.major)
+      .addClass(data.className);
+    $('.food-a').text(data.food);
+    $('.want-a').text(data.want);
+    $('.music-a').html(`
+      <p>${data.music}</p>
+      <iframe src="${data.iframe}"title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <p>${data.music_text}</p>`);
+    $('.kd-a').text(data.kd);
+    $('.free-a').text(data.free);
+
+
+
+
+
+    $('.rotate').on('click', function () {
+      $('.chara-image').toggleClass('rotate-on');
+      console.log('aaa');
+    });
+
+
+  }
 
 });
---
+++ js/set.js
@@ -0,0 +1,101 @@
+
+
+
+
+const chara_prof = {
+
+    "アンジェリカ": {
+
+        name: "アンジェリカ",
+        major: "Mov",
+        className: "mov",
+        food: "青リンゴサワー",
+        want: "カメラと幸せ",
+        music: "「Eve - お気に召すまま」",
+        music_text: "この曲はとても陽気で楽しいメロディーけど、歌詞はとても悲しい。MVの色やアニメーションも非常にポジティブで心地よいものけど、ストーリーはかなり悲しいものだと思う。このコントラストと、Eveの自己探求が好きです。",
+        iframe: "https://www.youtube.com/embed/nROvY9uiYYk?si=8Gn5tvuPYC6nd0Pe",
+        kd: "動画コースの機材場所（共創館6F）",
+        free: "『穿っては咲いた ブルーただこの間に割ってはランデブ』🎵🎵🎵"
+
+    },
+
+    "歌代": {
+
+        name: "うた",
+        major: "Web",
+        className: "web",
+        food: "はまち",
+        want: "ディズニーランドのチケット",
+        music: "「JO1 - Mad In Love」",
+        music_text: "きらきらした恋愛ソングで振り付けもかわいいです！",
+        iframe: "https://www.youtube.com/embed/Ouy9d6-r9nQ?si=7ee-Ajv3i_FXaXN2",
+        kd: "面白い先生が沢山いるところが魅力！",
+        free: "やる気、元気、純喜！！"
+
+    },
+
+    "志智": {
+
+        name: "アンジェリカ",
+        major: "Mov",
+        className: "mov",
+        food: "青リンゴサワー",
+        want: "カメラと幸せ",
+        music: "「Eve — お気に召すまま」",
+        music_text: "この曲はとても陽気で楽しいメロディーけど、歌詞はとても悲しい。MVの色やアニメーションも非常にポジティブで心地よいものけど、ストーリーはかなり悲しいものだと思う。このコントラストと、Eveの自己探求が好きです。",
+        iframe: "https://www.youtube.com/embed/nROvY9uiYYk?si=8Gn5tvuPYC6nd0Pe",
+        kd: "動画コースの機材場所（共創館6F）",
+        free: "『穿っては咲いた ブルーただこの間に割ってはランデブ』🎵🎵🎵"
+
+    },
+
+    "松本えり": {
+
+        name: "アンジェリカ",
+        major: "Mov",
+        className: "mov",
+        food: "青リンゴサワー",
+        want: "カメラと幸せ",
+        music: "「Eve — お気に召すまま」",
+        music_text: "この曲はとても陽気で楽しいメロディーけど、歌詞はとても悲しい。MVの色やアニメーションも非常にポジティブで心地よいものけど、ストーリーはかなり悲しいものだと思う。このコントラストと、Eveの自己探求が好きです。",
+        iframe: "https://www.youtube.com/embed/nROvY9uiYYk?si=8Gn5tvuPYC6nd0Pe",
+        kd: "動画コースの機材場所（共創館6F）",
+        free: "『穿っては咲いた ブルーただこの間に割ってはランデブ』🎵🎵🎵"
+
+    },
+
+    "岡村先生": {
+
+        name: "アンジェリカ",
+        major: "Mov",
+        className: "mov",
+        food: "青リンゴサワー",
+        want: "カメラと幸せ",
+        music: "「Eve — お気に召すまま」",
+        music_text: "この曲はとても陽気で楽しいメロディーけど、歌詞はとても悲しい。MVの色やアニメーションも非常にポジティブで心地よいものけど、ストーリーはかなり悲しいものだと思う。このコントラストと、Eveの自己探求が好きです。",
+        iframe: "https://www.youtube.com/embed/nROvY9uiYYk?si=8Gn5tvuPYC6nd0Pe",
+        kd: "動画コースの機材場所（共創館6F）",
+        free: "『穿っては咲いた ブルーただこの間に割ってはランデブ』🎵🎵🎵"
+
+    },
+
+
+
+
+
+    "アンジェリカ": {
+
+        name: "アンジェリカ",
+        major: "Mov",
+        className: "mov",
+        food: "青リンゴサワー",
+        want: "カメラと幸せ",
+        music: "「Eve — お気に召すまま」",
+        music_text: "この曲はとても陽気で楽しいメロディーけど、歌詞はとても悲しい。MVの色やアニメーションも非常にポジティブで心地よいものけど、ストーリーはかなり悲しいものだと思う。このコントラストと、Eveの自己探求が好きです。",
+        iframe: "https://www.youtube.com/embed/nROvY9uiYYk?si=8Gn5tvuPYC6nd0Pe",
+        kd: "動画コースの機材場所（共創館6F）",
+        free: "『穿っては咲いた ブルーただこの間に割ってはランデブ』🎵🎵🎵"
+
+    },
+
+};
